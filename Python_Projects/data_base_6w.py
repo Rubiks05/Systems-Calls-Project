ea71a77a8e3920781b6621c357b0031291b07334
Rubiks05 <antoniidaressurreicao@gmail.com>
2025-01-29 02:44:23 -0300

    Update

--- Python_Projects/data_base_6w.py
+++ Python_Projects/data_base_6w.py
@@ -9,7 +9,11 @@ for k in range(len(inputs)):
     #adicionando uma lista das 5 próximas chamadas ao sistema de cada(se tiver 5 adiante)
     next_elements = [k+i for i in range(1,7) if k+i < len(inputs)]
 
-    library[inputs[k]].append([inputs[i] for i in next_elements])
+    new_list = [inputs[i] for i in next_elements]
+
+    #Conferindo se já não existe uma lista igual:
+    if new_list not in library[inputs[k]]:
+        library[inputs[k]].append(new_list)
 
 for i in library:
     print("Call: {}".format(i))
