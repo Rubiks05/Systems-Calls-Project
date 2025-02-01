620167f31d0e17e318a8863494623bd0672863f0
Rubiks05 <antoniidaressurreicao@gmail.com>
2025-01-31 22:37:14 -0300

    Update

--- Python_Projects/data_base_5w.py
+++ Python_Projects/data_base_5w.py
@@ -67,12 +67,11 @@ for sc in range(len(inputs2)):
 #Estatísticas:
 percentual = 100.0 * (mismatches / lenght_of_sc)
 print("-----------------------------------------------------------------------------")
-print("O número total de systems calls analisadas é: {}".format(lenght_of_sc))
+print("O número total de systems calls analisadas é: {}".format(len(inputs1)))
 print("O número total de anomalias achadas é: {}".format(mismatches))
 print("O percentual de mismatches(anomalias) encontrado é: {:.2f}%".format(percentual))
 print("-----------------------------------------------------------------------------")
 #-------------------------------------------------------------------------------------------------------#
 
-print("Total de elementos no banco de dados:", len(inputs1))
 print("Total de elemento analisados:", len(inputs2))
 print("Total de janelas:{}".format(lenght_of_sc))
